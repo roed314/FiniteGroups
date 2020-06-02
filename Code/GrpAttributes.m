ad2243ab7f7e2f1d29a604cd88d13b52e2850f79
John Jones <jj@asu.edu>
2020-06-02 17:33:30 -0400

    Mergin MR functions

--- Code/GrpAttributes.m
+++ Code/GrpAttributes.m
@@ -317,3 +317,44 @@ intrinsic CommutatorCount(G::LMFDBGrp) -> Any
 
   return n;
 end intrinsic;
+
+
+intrinsic SylowSubgroups(G::LMFDBGrp) -> Any
+  {Compute SylowSubgroups of the group G}
+  GG := G`MagmaGrp;
+  SS := AssociativeArray();
+  F := FactoredOrder(GG);
+  for uu in F do 
+    p := uu[1];
+    SS[p] := SylowSubgroup(GG, p);
+  end for;
+  return SS;
+end intrinsic;
+
+intrinsic IsZGroup(G::LMFDBGrp) -> Any
+  {Check whether all the Syllowsubgroups are cylic}
+  SS := SylowSubgroups(G);
+  K := Keys(SS);
+  for k in K do
+    if not IsCyclic(SS[k]) then
+      return false;
+    end if;
+  end for;
+  return true;
+end intrinsic;
+
+intrinsic IsAGroup(G::LMFDBGrp) -> Any
+  {Check whether all the Syllowsubgroups are abelian}
+  SS := SylowSubgroups(G);
+  K := Keys(SS);
+  for k in K do
+    if not IsAbelian(SS[k]) then
+      return false;
+    end if;
+  end for;
+  return true;
+end intrinsic;
+
+
+
+
