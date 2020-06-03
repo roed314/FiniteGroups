b4a1840c693d0488ebe99257eb4e7b7a945cf7b9
Michael Bush <bushm@wlu.edu>
2020-06-03 14:55:17 -0400

    Small modifications/correction - use LMFDBGrp attributes correctly

--- mb_test.m
+++ mb_test.m
@@ -1,9 +1,9 @@
 intrinsic pgroup(G::LMFDBGrp) -> RngInt
     {1 if trivial group, p if order a power of p, otherwise 0}
-    if #G eq 1 then
+    if G`Order eq 1 then
 	return 1;
     else
-	fac := Factorization(#G);
+	fac := Factorization(G`Order);
         if #fac gt 1 then
 	   /* #G has more than one prime divisor. */ 
 	   return 0;
