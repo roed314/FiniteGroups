804cb8219311da61425f00ed0f18663831be5277
Michael Bush <bushm@wlu.edu>
2020-06-03 14:43:04 -0400

    Adding test file back

---
+++ mb_test.m
@@ -0,0 +1,16 @@
+intrinsic pgroup(G::LMFDBGrp) -> RngInt
+    {1 if trivial group, p if order a power of p, otherwise 0}
+    if #G eq 1 then
+	return 1;
+    else
+	fac := Factorization(#G);
+        if #fac gt 1 then
+	   /* #G has more than one prime divisor. */ 
+	   return 0;
+        else
+	    /* First component in fac[1] is unique prime divisor. */
+            return fac[1][1]; 
+	end if;
+    end if;
+end intrinsic;
+
