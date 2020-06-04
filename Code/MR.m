d9d0fffe88f315f6da6c5d9101c2e82e25610296
Manami Roy <manami.roy.90@gmail.com>
2020-06-04 12:09:14 -0400

    added subgroup attribute

--- Code/MR.m
+++ Code/MR.m
@@ -65,10 +65,10 @@ intrinsic split(H::LMFDBSubGrp) -> Any
   if not IsNormal(GG, HH) then
     return None();
   else 
-    comps := [el : el in S | el`order eq (Order(GG) div Order(HH))];
+    comps := [el : el in S | el`order eq (Order(GG) div Order(HH))]; 
     for s in comps do
       K := s`subgroup;
-      if #(HH meet K) eq 1 then
+      if #(HH meet K) eq 1 then 
         return true;
       end if;
     end for;
@@ -76,4 +76,67 @@ intrinsic split(H::LMFDBSubGrp) -> Any
   return dirbool;
 end intrinsic;
 
+intrinsic sylow(H::LMFDBSubGrp) -> RngIntElt
+{when order of H and order of Q are prime to each other it returns the radical of the order of H}
+  GG := H`MagmaAmbient;
+  HH := H`MagmaSubGrp;
+  Q := quo< GG | HH >;
+  if IsPrimePower(Order(HH)) and (Gcd(Order(HH), Order(Q)) eq 1) then
+    _, k , _ :=IsPrimePower(Order(HH));
+    return k;
+  else 
+    return 0;
+  end if;
+end intrinsic;
 
+intrinsic hall(H::LMFDBSubGrp) -> RngIntElt
+{when order of H and order of Q are prime to each other it returns the radical of the order of H}
+  GG := H`MagmaAmbient;
+  HH := H`MagmaSubGrp;
+  Q := quo< GG | HH >;
+  if Gcd(Order(HH), Order(Q)) eq 1 then
+    F:= Factorization(Order(HH));
+    return &*[f[1] : f in F];
+  end if;
+end intrinsic;
+
+intrinsic subgroup(H::LMFDBSubGrp) -> MonStgElt
+  {Determine label of subgroup}
+  GG := H`MagmaAmbient;
+  HH := H`MagmaSubGrp;
+  return label(H);
+end intrinsic;
+
+intrinsic subgroup_order(H::LMFDBSubGrp) -> RngIntElt
+  {returns order of the subgroup}
+  HH := H`MagmaSubGrp;
+  return Order(HH);
+end intrinsic;
+
+intrinsic ambient(H::LMFDBSubGrp) -> MonStgElt
+  {Determine label of the ambient group}
+  GG := H`MagmaAmbient;
+  return label(GG);
+end intrinsic;
+
+intrinsic ambient_order(H::LMFDBSubGrp) -> RngIntElt
+  {returns order of the ambient group}
+  GG := H`MagmaAmbient;
+  return Order(GG);
+end intrinsic;
+
+intrinsic quotient(H::LMFDBSubGrp) -> MonStgElt
+{Determine label of the quotient group}
+  GG := H`MagmaAmbient;
+  HH := H`MagmaSubGrp;
+  Q := quo< GG | HH >;
+  return label(Q);
+end intrinsic;
+
+intrinsic quotient_order(H::LMFDBSubGrp) -> RngIntElt
+{Determine the order of the quotient group}
+  GG := H`MagmaAmbient;
+  HH := H`MagmaSubGrp;
+  Q := quo< GG | HH >;
+  return label(Q);
+end intrinsic;
