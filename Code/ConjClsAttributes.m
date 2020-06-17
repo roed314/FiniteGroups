f8ab56d2c84a4da467b1bed266fdd8547f8c9593
Manami Roy <manami.roy.90@gmail.com>
2020-06-17 18:05:28 -0400

    Added functions to ConjClsAttributes

--- Code/ConjClsAttributes.m
+++ Code/ConjClsAttributes.m
@@ -63,3 +63,28 @@ intrinsic GetGrp(C::LMFDBGrpPermConjCls) -> Grp
     return C`Grp;
 end intrinsic;
 
+intrinsic degree(C::LMFDBGrpPermConjCls) -> Any
+    {the degree of the group, n in Sn}
+    CC := Get(C, "MagmaConjCls"); 
+    P:=Parent(CC[3]); // For a magma class CC, CC[3] gives a representative.
+    return Degree(P);
+end intrinsic;
+
+intrinsic centralizer(C::LMFDBGrpPermConjCls) -> Any
+  {Label for the isomorphism class of the centralizer of an element in this conjugacy class}
+  CC := Get(C, "MagmaConjCls");
+  C1:=Centralizer(Parent(CC[3]), CC[3]); //For a magma class CC, CC[3] gives a representative.
+  return label(C1);
+end intrinsic;
+
+intrinsic size(C::LMFDBGrpPermConjCls) -> RingIntElt
+  {Number of elements in this conjugacy class}
+  CC := Get(C, "MagmaConjCls");
+  return CC[2]; // For a magma class CC, CC[2] gives its size
+end intrinsic;
+
+intrinsic order(C::LMFDBGrpPermConjCls) -> RingIntElt
+  {Number of elements in this conjugacy class}
+  CC := Get(C, "MagmaConjCls");
+  return CC[1];// For a magma class CC, CC[1] gives the order of an element
+end intrinsic;
