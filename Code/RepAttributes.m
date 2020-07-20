7ea50efbdf968898ecca12acf3935e5319b9bfa6
Manami Roy <manami.roy.90@gmail.com>
2020-07-20 14:34:00 -0400

    added some intrinsics to RepAttributes

--- Code/RepAttributes.m
+++ Code/RepAttributes.m
@@ -136,3 +136,24 @@ intrinsic carat_label(G::LMFDBRepQQ) -> Any
    other software.}
   return None();
 end intrinsic;
+
+intrinsic order(H::LMFDBRepQQ) -> Any
+  {The size of the group}
+  return Order(H`MagmaGrp);
+end intrinsic;
+
+intrinsic group(H::LMFDBRepQQ) -> Any
+  {returns the LMFDB id for the abstract group}
+  return label(H`MagmaGrp);
+end intrinsic;
+
+
+intrinsic order(H::LMFDBRepZZ) -> Any
+  {The size of the group}
+  return Order(H`MagmaGrp);
+end intrinsic;
+
+intrinsic group(H::LMFDBRepZZ) -> Any
+  {returns the LMFDB id for the abstract group}
+  return label(H`MagmaGrp);
+end intrinsic;
