5ad01622014a5c984b50ffac0c70cc09b4c2b0f5
David Roe <roed.math@gmail.com>
2021-09-17 19:34:08 -0400

    Add timing to prep

--- Code/AutPrep.m
+++ Code/AutPrep.m
@@ -8,9 +8,11 @@ procedure PrepSmallGroup(N, i)
     G := NewLMFDBGrp(SmallGroup(N, i), Sprintf("%o.%o", N, i));
     if Type(G`MagmaGrp) eq GrpPC then
         AssignBasicAttributes(G);
+        t0 := Cputime();
         RePresent(G : reset_attrs:=false);
+        t := Cputime() - t0;
         F := Open("autsolv_prep/" * Proc * ".txt", "w");
-        Write(F, Sprintf("<PCGroup(%o), %o>", CompactPresentation(G`MagmaGrp), G`gens_used));
+        Write(F, Sprintf("<PCGroup(%o), %o, %o>", CompactPresentation(G`MagmaGrp), G`gens_used, t));
         Flush(F);
     end if;
 end procedure;
