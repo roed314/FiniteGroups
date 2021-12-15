55a82bacc0097c96cdbe612041c37960841e6711
David Roe <roed.math@gmail.com>
2021-12-15 17:18:36 -0500

    Another file for computing transitive group hashes

---
+++ Code/AddTHashes2.m
@@ -0,0 +1,8 @@
+AttachSpec("hashspec");
+
+// cat DATA/hash/needhash.txt | parallel -j64 magma nTt:={1} AddTHashes2.m
+
+SetColumns(0);
+n, t := Explode([StringToInteger(c) : c in Split(nTt, "T")]);
+G := TransitiveGroup(n, t);
+PrintFile("DATA/hash/trun.extra/" * nTt, Sprint(hash(G)));
