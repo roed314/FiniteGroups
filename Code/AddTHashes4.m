99936d1e0d091c07f071fc19e60f497c2567b4ca
David Roe <roed.math@gmail.com>
2022-02-03 23:24:35 -0500

    New file for using IsIsomorphic

---
+++ Code/AddTHashes4.m
@@ -0,0 +1,47 @@
+AttachSpec("hashspec");
+
+// ls DATA/hashclusters/active | parallel -j128 --timeout 3600 magma hsh:="{1}" AddTHashes4.m
+
+SetColumns(0);
+nTt_lookup := AssociativeArray();
+G_lookup := AssociativeArray();
+nTts := [];
+activefile := "DATA/hashclusters/active/" * hsh;
+for cluster in Split(Read(activefile), "\n") do
+    first := Split(cluster, " ")[1];
+    nTt_lookup[first] := cluster;
+    n, t := Explode([StringToInteger(c) : c in Split(first, "T")]);
+    G_lookup[first] := TransitiveGroup(n, t);
+    Append(~nTts, first);
+end for;
+while #nTts gt 0 do
+    t0 := Cputime();
+    G := G_lookup[nTts[1]];
+    label := nTts[1];
+    cluster := [nTt_lookup[label]];
+    nTts := nTts[2..#nTts];
+    i := 1;
+    while i le #nTts do
+        now := nTts[i];
+        H := G_lookup[now];
+        print "Checking isomorphism", label, now;
+        if IsIsomorphic(G, H) then
+            Append(~cluster, nTt_lookup[now]);
+            Remove(~nTts, i);
+        else
+            i +:= 1;
+        end if;
+    end while;
+    // Since this process may get killed, we want to write output now
+    print "Writing progress";
+    PrintFile("DATA/hashclusters/isotest.times/" * hsh, Sprintf("%o size %o(%o), %o", label, #cluster, &+[#Split(x, " ") : x in cluster], Cputime() - t0));
+    PrintFile("DATA/hashclusters/isotest_finished/" * hsh * "." * label, Join(cluster, " "));
+    if #nTts gt 0 then
+        tmp := "DATA/hashclusters/tmp/" * hsh;
+        PrintFile(tmp, Join([nTt_lookup[nTt] : nTt in nTts], "\n"));
+        System("mv " * tmp * " " * activefile); // mv is atomic
+    else
+        System("rm " * activefile);
+    end if;
+end while;
+exit;
