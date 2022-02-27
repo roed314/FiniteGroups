cff253bd6df7568f9591024e0c7fc1a6fe6e7cd4
David Roe <roed.math@gmail.com>
2022-02-27 14:45:15 -0500

    Better fix for whitespace problem

--- Code/IterativeSubgroupSetup.m
+++ Code/IterativeSubgroupSetup.m
@@ -1,8 +1,6 @@
 // Given a transitive group as input, set up the files used to find all core-free subgroups of specific indexes (40 or 44).  Used in trying to resolve the last few outstanding cases.
 
-if nTt[1] eq "\n" then
-    nTt := nTt[2..#nTt];
-end if;
+SetColumns(0);
 n, t := Explode([StringToInteger(c) : c in Split(nTt, "T")]);
 G := TransitiveGroup(n, t);
 P := SylowSubgroup(G, 2);
@@ -19,23 +17,33 @@ end if;
 for i in [1..#Hs] do
     slab := nTt * "." * Sprint(i);
     F := "DATA/hashclusters/last/" * slab * ".m";
+    PrintFile(F, "SetColumns(0);");
+    PrintFile(F, Sprintf("checkfile := \"DATA/hashclusters/lastdone/%o\";", slab));
+    PrintFile(F, "file_exists, ifile := OpenTest(checkfile, \"r\");");
+    PrintFile(F, "if file_exists then");
+    PrintFile(F, "    print \"Already done\";");
+    PrintFile(F, "    exit;");
+    PrintFile(F, "end if;");
     PrintFile(F, Sprintf("G := TransitiveGroup(%o, %o);", n, t));
     PrintFile(F, Sprintf("H := %m;", Hs[i]));
     PrintFile(F, "t0 := Cputime();");
     PrintFile(F, Sprintf("S := [K`subgroup : K in Subgroups(H : IndexEqual:=%o)];", m));
-    PrintFile(F, "PrintFile(\"../last.times\", Sprintf(\"Found %o subgroups within " * slab * " in %o\", #S, Cputime() - t0));");
+    PrintFile(F, "PrintFile(\"DATA/hashclusters/last.times\", Sprintf(\"Found %o subgroups within " * slab * " in %o\", #S, Cputime() - t0));");
     PrintFile(F, "t0 := Cputime();");
+    PrintFile(F, "c := 0;");
     PrintFile(F, "for K in S do");
     PrintFile(F, "    if #Core(G, K) eq 1 then");
+    PrintFile(F, "        c +:= 1;");
     PrintFile(F, "        t := TransitiveGroupIdentification(Image(CosetAction(G, K)));");
     PrintFile(F, Sprintf("        if t eq %o then", t));
-    PrintFile(F, "            PrintFile(\"../last.selfsib/"*nTt*"\", Sprintf(\"%o\", Generators(K)));");
+    PrintFile(F, "            PrintFile(\"DATA/hashclusters/last.selfsib/"*nTt*"\", Sprintf(\"%o\", Generators(K)));");
     PrintFile(F, "        else");
-    PrintFile(F, "            PrintFile(\"../last.osib/"*nTt*"\", Sprintf(\"%o\", Generators(K)));");
+    PrintFile(F, "            PrintFile(\"DATA/hashclusters/last.osib/"*nTt*"\", Sprintf(\"%o\", Generators(K)));");
     PrintFile(F, "        end if;");
     PrintFile(F, "    end if;");
     PrintFile(F, "end for;");
-    PrintFile(F, "PrintFile(\"../last.times\", Sprintf(\"Finished checking %o subgroups within " * slab * " in %o\", #S, Cputime() - t0));");
+    PrintFile(F, "PrintFile(\"DATA/hashclusters/last.times\", Sprintf(\"Finished checking %o subgroups within " * slab * " in %o\", #S, Cputime() - t0));");
+    PrintFile(F, "PrintFile(checkfile, Sprint(c));");
     PrintFile(F, "exit;");
 end for;
 exit;
