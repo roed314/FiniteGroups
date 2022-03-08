f0f14a9afd977ba05cd1da9d1ed5a9b089cbbcc4
David Roe <roed.math@gmail.com>
2022-03-08 03:42:17 -0500

    Only change group if it decreases degree

--- Code/MinReps3.m
+++ Code/MinReps3.m
@@ -1,4 +1,4 @@
-// cat DATA/hashclusters/transitive_add.txt | parallel -j128 magma gp:="{1}" MinReps3.m
+// cat DATA/hashclusters/transitive_add_imp.txt | parallel -j128 magma gp:="{1}" MinReps3.m
 
 AttachSpec("spec");
 SetColumns(0);
@@ -8,6 +8,10 @@ n, t := Explode([StringToInteger(c) : c in Split(nTt, "T")]);
 G := TransitiveGroup(n, t);
 t0 := Cputime();
 phi, P := MinimalDegreePermutationRepresentation(G);
+if Degree(P) eq n then
+    // Just use the original group
+    P := G;
+end if;
 PrintFile("DATA/minreps/" * label, Sprintf("%o|%o|%o", nTt, Degree(P), SavePerms([g : g in Generators(P)])));
 PrintFile("DATA/minrep.timings/" * label, Cputime() - t0);
 exit;
