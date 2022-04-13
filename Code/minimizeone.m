329cd5eb5bab3ee44ebee6836c7e930c1ac1dd90
David Roe <roed.math@gmail.com>
2022-04-13 15:42:14 -0400

    Add new minimizing script

---
+++ Code/minimizeone.m
@@ -0,0 +1,13 @@
+// Call MinimalDegreePermutationRepresentation when needed on the output of autone.m
+// Usage: cat important.txt | parallel -j 64 --timeout 1200 magma -b data:="{1}" minimiazeone.m > aut_finished.txt
+
+SetColumns(0);
+AttachSpec("hashspec");
+pieces := Split(data, " ");
+if "T" in pieces[#pieces] then
+    print data;
+else
+    G := StringToGroup(pieces[#pieces]);
+    M := Image(MinimalDegreePermutationRepresentation(G));
+    print Join(pieces[1..#pieces-1], " ") * " " * GroupToString(M);
+end if;
