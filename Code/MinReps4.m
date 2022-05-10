41259b6d42e58de93deb8e19c9289c0194727228
David Roe <roed.math@gmail.com>
2022-05-10 14:50:30 -0400

    Working on MinReps4.m

--- Code/MinReps4.m
+++ Code/MinReps4.m
@@ -11,20 +11,39 @@ if done then
     exit;
 end if;
 desc := Read(infile);
-G := StringToGroup(desc);
+G0 := StringToGroup(desc);
+G := G0;
 t0 := Cputime();
-phi, P := MinimalDegreePermutationRepresentation(G);
-if Type(G) eq GrpPerm and Degree(P) eq Degree(G) then
-    // Just use the original group
-    P := G;
-    phi := IdentityHomomorphism(G);
-    gens := Generators(G);
-elif Type(G) eq GrpPC then
-    gens := PCGenerators(G);
+if Type(G0) eq GrpMat and not IsField(CoefficientRing(G0)) then
+    // MinimalDegreePermutationRepresentation fails
+    if IsSolvable(G0) then
+        G, psi := PCGroup(G0);
+    else
+        V := RSpace(G0);
+        x := Random(V);
+        T := {x};
+        psi, G, K := OrbitAction(G0, T);
+        while #K ne 1 do
+            x := Random(V);
+            Include(~T, x);
+            psi, G, K := OrbitAction(G0, T);
+        end while;
+    end if;
 else
-    gens := Generators(G);
+    psi := IdentityHomomorphism(G);
 end if;
-PrintFile(outfile, Sprintf("%o|%o|%o", desc, Degree(P), SavePerms([phi(g) : g in gens])));
+phi, P := MinimalDegreePermutationRepresentation(G);
+if Type(G0) eq GrpPerm and Degree(P) eq Degree(G0) then
+    // Just use the original group
+    P := G0;
+    phi := IdentityHomomorphism(G0);
+    gens := Generators(G);
+elif Type(G0) eq GrpPC then
+    gens := PCGenerators(G0);
+else
+    gens := Generators(G0);
+end if;
+PrintFile(outfile, Sprintf("%o|%o|%o", desc, Degree(P), SavePerms([phi(psi(g)) : g in gens])));
 PrintFile(timefile, Cputime() - t0);
 System("rm " * infile);
 exit;
