b2a03b936a5088113a4a19fae15c02d8603fae2b
David Roe <roed.math@gmail.com>
2022-06-20 12:41:30 -0700

    Another script for setting up the pc group generation code by making permutation group versions of matrix groups for input

---
+++ Code/PrepPCInput.m
@@ -0,0 +1,12 @@
+// ls DATA/pcrep.toprep/ | parallel -j76 magma -b label:="{1}" PrepPCInput.m
+
+AttachSpec("spec");
+SetColumns(0);
+desc := Read("DATA/pcrep.toprep/" * label);
+G := StringToGroup(desc);
+t0 := Cputime();
+Q, phi := MyQuotient(G, sub<G|> : max_orbits:=8, num_checks:=8);
+PrintFile("DATA/pcrep.todo/" * label, GroupToString(Q));
+PrintFile("DATA/pcrep.preptimings/" * label, Sprintf("%o %o %o", Cputime() - t0, Degree(Q), desc));
+System("rm DATA/pcrep.toprep/" * label);
+exit;
