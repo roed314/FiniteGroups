ae5f21c067956d5b151f55105515479b7f9f36bd
David Roe <roed.math@gmail.com>
2022-09-23 16:51:46 -0400

    Use spec rather than hashspec

--- Code/GLqSmallSubs.m
+++ Code/GLqSmallSubs.m
@@ -1,7 +1,7 @@
 // Usage parallel -j 19 "magma -b q:={1} GLqSmallSubs.m > DATA/GLq{1}.txt" ::: 4 8 9 16 25 27 32 49 64 81 121 125 128 169 243 256 289 343 361
 
 SetColumns(0);
-AttachSpec("hashspec");
+AttachSpec("spec");
 
 G := GL(2, StringToInteger(q));
 for H in Subgroups(G) do
