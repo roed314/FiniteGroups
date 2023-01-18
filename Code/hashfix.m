896078ea2829fdf51cf109f46c9a6228f710ae12
David Roe <roed.math@gmail.com>
2023-01-18 16:45:41 -0500

    Add timing info

--- Code/hashfix.m
+++ Code/hashfix.m
@@ -1,10 +1,12 @@
-// Usage: ls hashfix | parallel -j 80 magma -b label:={1} hashfix.m
+// Usage: ls DATA/hashfix.todo | parallel -j 80 magma -b label:={1} hashfix.m
 
 SetColumns(0);
 AttachSpec("hashspec");
 
 desc := Split(Read("DATA/hashfix.todo/" * label), "\n")[1];
 G := StringToGroup(desc);
+t0 := Cputime();
 hsh := hash(G);
 Write("DATA/hashfix.out/" * label, Sprint(hsh) : Overwrite);
+Write("DATA/hashfix.timings/" * label, Sprint(Cputime() - t0) : Overwrite);
 exit;
