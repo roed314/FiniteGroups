3c45ea4dd89974eabef79747032c9910023e7960
David Roe <roed.math@gmail.com>
2023-01-21 00:28:19 -0500

    Working on computing lattice in the cloud

--- Code/cloud_compute.py
+++ Code/cloud_compute.py
@@ -6,9 +6,10 @@
 
 import os
 import re
+import time
 import argparse
 import subprocess
-# defhikpruvxyz
+from collections import defaultdict
 opj = os.path.join
 ope = os.path.exists
 err_location_re = re.compile(r'In file "(.*)", line (\d+), column (\d+):')
@@ -38,7 +39,12 @@ def set_preload(label, attr, value):
     with open(fname, "w") as Fout:
         _ = Fout.write("|".join(attrs) + "\n" + "|".join(values) + "\n")
 
-def run(label, codes, timeout):
+def read_tmpheader(name):
+    with open(name + ".tmpheader") as F:
+        code, cols = F.read().strip().split("\n")
+        return cols.split("|")
+
+def run(label, codes, timeout, subgroup_index_bound):
     subprocess.run('parallel -n0 --timeout %s "magma -b label:=%s codes:=%s ComputeCodes.m >> DATA/errors/%s 2>&1" ::: 1' % (timeout, label, codes, label), shell=True)
     # Move timing and error information to the common output file and extract timeout and error information
     sublines = []
@@ -66,6 +72,15 @@ def run(label, codes, timeout):
                     if line and line[0] in finished:
                         if line[0] == "S":
                             sublines.append(line)
+                        elif line[0] == "s":
+                            # Extract the subgroup index bound for use in computing lattice x-values
+                            sheader = read_tmpheader("sub")
+                            sdata = line.strip().split("|")[1:]
+                            subgroup_index_bound = dict(zip(sheader, sdata))["subgroup_index_bound"]
+                            if subgroup_index_bound == r"\N":
+                                subgroup_index_bound = None
+                            else:
+                                subgroup_index_bound = int(subgroup_index_bound)
                         _ = Fout.write(line)
             os.unlink(o) # Remove output so it's not copied multiple times
         e = opj("DATA", "errors", label)
@@ -87,35 +102,134 @@ def run(label, codes, timeout):
                 if loc is None:
                     loc = known
             os.unlink(e) # Remove errors so that they're not copied multiple times
-    return done, finished, time_used, last_time_line, loc, sublines
+    return done, finished, time_used, last_time_line, loc, sublines, subgroup_index_bound
 
 standard_label_re = re.compile(r"(\d+\.[a-z]+\d+)(\.[a-z]+\d+)?")
+normal_label_re = re.compile(r"(\d+\.[a-z]+\d+)(\.[a-z]+\d+)?\.N")
 def standard_label(label):
     return standard_label_re.fullmatch(label)
-def aut_label(label):
-    return standard_label(label).group(1)
-def aut_graph(sdata):
+def aut_label(label, normal):
+    m = standard_label(label)
+    if m:
+        return m.group(1)
+    if normal:
+        m = normal_label_re.fullmatch(label)
+        if m:
+            return m.group(1)
+def aut_graph(sdata, normal=False):
     final = defaultdict(set)
     for sdatum in sdata:
-        alabel = aut_label(sdatum["label"])
-        final[alabel].update([aut_label(lab) for lab in sdatum[3]])
+        alabel = aut_label(sdatum["short_label"], normal)
+        if alabel:
+            if normal:
+                contains = sdatum["normal_contains"]
+            else:
+                contains = sdatum["contains"]
+            # This will handle the case that contains="{}" ok, since aut_label will return None for [""]
+            contains = [aut_label(lab, normal) for lab in contains[1:-1].split(",")]
+            final[alabel].update([lab for lab in contains if lab is not None])
+    # Make sure that every "contains" node is in final
+    for alabel, contains in list(final.items()):
+        final[alabel] = set(lab for lab in contains if lab in final)
     adata = []
     seen = set()
     for sdatum in sdata:
-        alabel = aut_label(sdatum[0])
-        if alabel in seen: continue
-        new_datum = list(sdatum)
-        new_datum[0] = alabel
-        new_datum[3] = final[alabel]
-        adata.append(new_datum)
+        alabel = aut_label(sdatum["short_label"], normal)
+        if alabel is None or alabel in seen: continue
+        adata.append({"label": alabel,
+                      sdatum["subgroup_tex"],
+                      "order": int(sdatum["subgroup_order"]),
+                      "contains": final[alabel]})
     return adata
 
-def compute_diagramx(label, sublines):
-    with open("subagg1.tmpheader") as F:
-        code, cols = F.read().strip().split("\n")
-        cols = cols.split("|")
+def get_graph(sdata, normal=False):
+    final = {}
+    for sdatum in sdata:
+        if normal:
+            contains = sdatum["normal_contains"]
+        else:
+            contains = sdatum["contains"]
+        if len(contains) > 2: # {}
+            final[sdatum["short_label"]] = contains[1:-1].split(",")
+        else:
+            final[sdatum["short_label"]] = []
+    # Make sure that every "contains" node is in final
+    for label, contains in list(final.items()):
+        final[label] = set(lab for lab in contains if lab in final)
+    return [{"label": sdatum["short_label"],
+             "tex": sdatum["subgroup_tex"],
+             "order": int(sdatum["subgroup_order"]),
+             "contains": final[sdatum["short_label"]]} for sdatum in sdata]
+
+def layout_graph(label, graph, rank_by_order=True):
+    nodes = []
+    edges = []
+    ranks = defaultdict(list)
+    for rec in graph:
+        lab, tex, contains = rec["label"], rec["tex"], ",".join(rec["contains"])
+        nodes.append(f'"{lab}" [label="{tex}",shape=plaintext]')
+        if contains:
+            edges.append(f'"{lab}" -> {{"{contains}"}} [dir=none]')
+        ranks[rec["order"]].append(lab)
+    if rank_by_order:
+        R = sorted(ranks)
+        for i in range(len(R)):
+            order = R[i]
+            nodes.append(f'"order{order}" [style="invis"]')
+            if i > 0:
+                edges.append(f'"order{order}" -> "order{R[i-1]}" [style="invis"]')
+            ranks[order].append(f'order{order}')
+    nodes = ";\n".join(nodes)
+    edges = ";\n".join(edges)
+    ranks = ";\n".join('{rank=same; "%s"}' % ('" "'.join(labs)) for labs in ranks.values())
+    graph = f"""strict digraph "{label}" {{
+rankdir=TB;
+splines=line;
+{edges};
+{nodes};
+{ranks};
+}}
+"""
+    infile = f"/tmp/graph{label}.in"
+    outfile = f"/tmp/graph{label}.out"
+    with open(infile, "w") as F:
+        _ = F.write(graph)
+    t = time.time()
+    subprocess.run(["dot", "-Tplain", "-o", outfile, infile], check=True)
+    xcoord = {}
+    with open(outfile) as F:
+        maxx = 0
+        minx = 10000
+        for line in F:
+            if line.startswith("graph"):
+                scale = float(line.split()[2])
+            elif line.startswith("node"):
+                pieces = line.split()
+                short_label = pieces[1].replace('"', '')
+                if not short_label.startswith("order"):
+                    diagram_x = int(round(10000 * float(pieces[2]) / scale))
+                    xcoord[short_label] = diagram_x
+                    if diagram_x > maxx:
+                        maxx = diagram_x
+                    if diagram_x < minx:
+                        minx = diagram_x
+    if rank_by_order:
+        # We have to remove the phantom nodes used to set the ranks
+        margin = min(minx, 10000-maxx)
+        minx -= margin
+        maxx += margin
+        rescale = 10000 / (maxx - minx)
+        for short_label, x in list(xcoord.items()):
+            xcoord[short_label] = int(round((x - minx) * rescale))
+    os.remove(infile)
+    os.remove(outfile)
+    return xcoord
+
+def compute_diagramx(label, sublines, subgroup_index_bound):
+    cols = read_tmpheader("subagg1")
     with open("output", "a") as F:
         subs = []
+        norms = []
         for line in sublines:
             vals = line.strip().split("|")
             if vals[0] != "S" + label:
@@ -126,17 +240,29 @@ def compute_diagramx(label, sublines):
                 _ = F.write(f"E{label}|Compute diagramx-length mismatch {len(vals)} vs {len(cols)}")
                 continue
             lookup = dict(zip(cols, vals))
-            # Omit subgroups that aren't labeled normally (past the index bound)
-            if standard_label(lookup["label"]):
-                subs.append(lookup)
-            else:
-                pass
+            # Omit subgroups that have unusual labels (they're past the index bound)
+            if standard_label(lookup["short_label"]):
+                if subgroup_index_bound in [None, 0] or lookup["quotient_order"] != r"\N" and int(lookup["quotient_order"]) <= subgroup_index_bound:
+                    subs.append(lookup)
+                if lookup["normal"] == "t":
+                    norms.append(lookup)
+            elif lookup["short_label"].endswith(".N"): # except that we want the normal ones
+                norms.append(lookup)
             #sdatum[2] = int(sdatum[2]) # subgroup_order
             #sdatum[3] = sdatum[3][1:-1].split(",") # contains
             #sdatum[4] = (sdatum[4] == 't') # normal
             #sdatum[5] = (sdatum[5] == 't') # outer_equivalence
-        out_equiv = (subs[0]["outer_equivalence"] == "t")
-        
+    out_equiv = (subs[0]["outer_equivalence"] == "t")
+    if out_equiv:
+        graphs = [get_graph(subs), get_graph(norms)]
+    else:
+        graphs = [aut_graph(subs), aut_graph(norms, normal=True), get_graph(subs), get_graph(norms, normal=True)]
+    xcoords = ([layout_graph(label, graph, rank_by_order=True) for graph in graphs] +
+               [layout_graph(label, graph, rank_by_order=False) for graph in graphs])
+    with open("output", "a") as F:
+        for slabel in sublines:
+            diagramx = "{" + ",".join([str(D.get(slabel, -1)) for D in xcoords]) + "}"
+            _ = F.write(f"D{label}|{label}|{slabel}|{diagramx}\n")
 
 with open("DATA/manifest") as F:
     for line in F:
@@ -172,10 +298,11 @@ with open("DATA/manifest") as F:
             # You can call tmpheaders(summarize=True) from cloud_collect.py to get a summary of the codes
             codes = "blajJzcCrqQsSLhtguomw" # Note that D = subagg3 (diagramx) is skipped since it's filled in below
             skipped = ""
+            subgroup_index_bound = None
             while codes:
-                done, finished, time_used, last_time_line, err, sublines = run(label, codes, timeout)
+                done, finished, time_used, last_time_line, err, sublines, subgroup_index_bound = run(label, codes, timeout, subgroup_index_bound)
                 if sublines:
-                    compute_diagramx(label, sublines) # writes to output
+                    compute_diagramx(label, sublines, subgroup_index_bound) # writes directly to output
                 if done: break
                 for code in finished:
                     codes = codes.replace(code, "")
