04334beaec7f2062b3e001b6cf5d0dfcf3dae634
David Roe <roed.math@gmail.com>
2023-01-21 00:43:19 -0500

    Compute covers for all projective groups of Lie type

---
+++ Code/LieCover.m
@@ -0,0 +1,10 @@
+// Usage ls DATA/LieCovers.todo | parallel -j120 magma -b label:={1} LieCover.m
+
+AttachSpec("spec");
+SetColumns(0);
+infile := "DATA/LieCovers.todo/" * label;
+outfile := "DATA/homs/" * label;
+desc := Read(infile);
+edesc := PStringToHomString(desc);
+Write(outfile, edesc : Overwrite);
+exit;
