461a5f13422dd4e50c82d25ad3aece3cbefb7445
David Roe <roed.math@gmail.com>
2023-01-22 20:26:21 -0500

    Fix bug with group homs

--- Code/CCcount.m
+++ Code/CCcount.m
@@ -1,11 +1,19 @@
-// Usage: ls DATA/descriptions | parallel -j120 magma -b label:={1} CCcount.m
+// Usage: ls DATA/descriptions | parallel -j120 --timeout 2 magma -b label:={1} CCcount.m
 
 AttachSpec("spec");
 SetColumns(0);
 infile := "DATA/descriptions/" * label;
 outfile := "DATA/cccount/" * label;
 
-G := StringToGroup(Read(infile));
+s := Read(infile);
+if "--" in s then
+    if ">>" in s then
+        s := PySplit(s, "-->>")[2];
+    else
+        s := PySplit(s, "--")[1];
+    end if;
+end if;
+G := StringToGroup(s);
 n := Nclasses(G);
 Write(outfile, Sprint(n) : Overwrite);
 exit;
