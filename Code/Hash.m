30b980c1441b99b52d76036c4167b5deb2803a50
David Roe <roed.math@gmail.com>
2023-02-11 00:13:43 +0100

    Use identify_local instead

--- Code/Hash.m
+++ Code/Hash.m
@@ -452,7 +452,7 @@ intrinsic IdentifyGroups(Glist::SeqEnum : hashes:=0) -> SeqEnum
         F := Open(fname, "w");
         Write(F, Join(toid, "\n"));
         delete F;
-        System(Sprintf("./identify.py --input %o --output %o.out", fname, fname));
+        System(Sprintf("./identify_local.py --input %o --output %o.out", fname, fname));
         possibilities := [[<StringToInteger(c) : c in Split(label, ".")> : label in Split(x, "|")] : x in Split(Read(fname * ".out"), "\n") | #x gt 0];
         if #possibilities ne #toid then
             print "Identify script did not produce the correct number of possibilities", fname;
