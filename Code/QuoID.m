12e292907ba7069dc8879574f756edc7c462521a
David Roe <roed.math@gmail.com>
2023-06-29 14:10:09 -0400

    Add scripts for identifying subgroups and quotients

---
+++ Code/QuoID.m
@@ -0,0 +1,30 @@
+// Usage: ls /scratch/grp/smallid/quoin/ | parallel -j120 --timeout 120 magma -b base:=/scratch/grp/smallid/ label:={1} QuoID.m
+// Needs to have DATA/descriptions set
+
+AttachSpec("spec");
+SetColumns(0);
+
+if base[#base] ne "/" then
+    base := base * "/";
+end if;
+iname := base * "quoin/" * label;
+oname := base * "quoout/" * label;
+done, F := OpenTest(ofile, "r");
+if done then
+    print "Already complete";
+    quit;
+end if;
+
+desc := Read("DATA/descriptions/" * label);
+G := MakeBigGroup(desc, label : preload:=true);
+
+subs := Split(Read(iname), "\n");
+for H in subs do
+    short_label, gens := Explode(Split(H, "|"));
+    gens := Split(gens, ",");
+    HH := sub<G`MagmaGrp | [LoadElt(g, G) : g in gens]>;
+    Q := quo<G`MagmaGrp | HH>;
+    Hid := IdentifyGroup(Q);
+    PrintFile(oname, Sprintf("%o.%o|%o.%o", label, short_label, Hid[1], Hid[2]));
+end for;
+quit;
