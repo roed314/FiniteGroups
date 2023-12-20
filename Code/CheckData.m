7f982f1e81744b23657d0022f707a27fea3648ec
David Roe <roed.math@gmail.com>
2023-12-20 15:58:05 -0800

    Try dealing with runs that have no subgroup data

--- Code/CheckData.m
+++ Code/CheckData.m
@@ -188,10 +188,10 @@ for pair in data["J"] do
         end try;
     end for;
 end for;
-if &or{#rtypes ne 1 : run -> rtypes in acceptable} then
-    zero := Join([run : run -> rtypes in acceptable | # rtypes eq 0], ",");
-    big := Join([run : run -> rtypes in acceptable | # rtypes gt 1], ",");
-    valid := Join([Join([rtype : rtype in rtypes], ",") : run -> rtypes in acceptable | #rtypes gt 1], ";");
+if &or{#acceptable[run] ne 1 : run in sruns} then
+    zero := Join([run : run in sruns | #acceptable[run] eq 0], ",");
+    big := Join([run : run in sruns | #acceptable[run] gt 1], ",");
+    valid := Join([Join([rtype : rtype in rtypes], ",") : run in sruns | #acceptable[run] gt 1], ";");
     PrintFile(errfile, Sprintf("%o|8|%o|%o|%o", label, zero, big, valid)); // err 8
     exit;
 end if;
